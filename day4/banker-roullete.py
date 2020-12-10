d9871658ab00a9bbd9aeef7b1a2fc9643c6828cd
Joshua-Petrey <JoshuaPetrey@protonmail.com>
2020-12-10 13:47:45 -0500

    added comments

--- day4/banker-roullete.py
+++ day4/banker-roullete.py
@@ -1,6 +1,7 @@
 import random
-
-people = input("Type some names").split(", dave, mike")
+# make a list of names
+people = input("Type some names").split(", ")
+# grab random name
 payer = random.randint(0, len(people))
 
 print(f"{people[payer]} is going to buy the meal today!")
