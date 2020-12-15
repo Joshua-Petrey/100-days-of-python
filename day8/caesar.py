fa920881778f20090ea09aebcaed53e2ac67777f
Joshua-Petrey <JoshuaPetrey@protonmail.com>
2020-12-14 20:03:34 -0500

    updated caesar.py

--- day8/caesar.py
+++ day8/caesar.py
@@ -1,24 +1,31 @@
-from typing import cast
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+should_end = False
+while not should_end:
 
-method = input("Type 'encode' to encrypt and 'decode' to decrypt.\n")
-message = input("Type your message here.\n").lower()
-shift = int(input("What is the shift?\n"))
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  # loop back if the user enters a shift that is larger than the alphabet length
+  shift = shift % 26
 
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-def caesar(method, message, shift_amount):
-
-    end_text = ""
-    for letter in message:
-        position = alphabet.index(letter)
-        new_position = (position + shift_amount) % 26
-        if method == 'decode':
-        	new_position = (position - shift_amount) % 26
-        new_letter = alphabet[new_position]
-        end_text += new_letter
-    return end_text
-
-print(caesar(method, message, shift))
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+    
