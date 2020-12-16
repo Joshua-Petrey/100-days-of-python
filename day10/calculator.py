951d25652559026f7bd90fffe2cbdff961916226
Joshua-Petrey <JoshuaPetrey@protonmail.com>
2020-12-16 13:23:05 -0500

    day 10 complete

---
+++ day10/calculator.py
@@ -0,0 +1,50 @@
+from art import logo
+def add(n1, n2):
+	'''Returns the sum of the two args'''
+	return n1 + n2
+
+def sub(n1, n2):
+	'''Returns the difference of the two args'''
+	return n1 - n2
+
+def mult(n1, n2):
+	'''Returns the product of the two args'''
+	return n1 * n2
+
+def div(n1, n2):
+	'''Returns the quotient of the two args'''
+	return n1 / n2
+
+operations = {
+	"+": add,
+	"-": sub,
+	"*": mult,
+	"/": div,
+}
+
+def calculator():
+	print(logo)
+	num1 = float(input("Enter the first number.\n"))
+
+	for symbol in operations:
+		print(symbol)
+
+	should_continue = True
+	while should_continue:
+
+		op = input("What operation do you want to perform?\n")
+		num2 = float(input("Enter another number.\n"))
+		calc_function = operations[op]
+		answer = calc_function(num1, num2)
+
+		print(f"{num1} {op} {num2} = {answer}")
+		
+		if input(f"Type 'y' to keep calulating with {answer}, or 'n' to start new calculation ") == 'y':
+			num1 = answer
+		else:
+			# if users types anything but 'y' exit the loop and recursively call calculator() again
+			# to enter a new num1 
+			should_continue = False
+			calculator()
+
+calculator()
---
+++ day10/scratch.py
@@ -0,0 +1,26 @@
+def times():
+	result = 3 * 5
+	return result
+
+
+#Doc strings
+def formatName(fname, lname):
+	''' Take two strings: fname and lname. Outputs the the arguments as 
+	a formatted string that is in title case '''
+	formatted_fname = fname.title()
+	formatted_lname = lname.title()
+	return f"{formatted_fname} {formatted_lname}"
+
+
+print(formatName("jOShuA", "PeTrEy"))
+long = len(formatName("jghfg", "poprtter"))
+print(long)
+
+# functions can be  defined in other functions
+def outer_function(a, b):
+    def inner_function(c, d):
+        return c + d
+    return inner_function(a, b)
+ 
+result = outer_function(5, 10)
+print(result)
